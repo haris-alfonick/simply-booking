ffabd375934100c3e3064e1d191027cd384cf39d
raheemalfonick <raheem.alfonick@gmail.com>
2026-01-03 20:55:32 +0500

    create react setup and create home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './component/Homepage';
+
 
 const App = () => {
   return (
